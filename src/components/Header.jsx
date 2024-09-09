548468ad0b1f6c76b26e44a27e66342f00ce93db
theamit00 <amitmr520@gmail.com>
2024-09-09 12:40:53 +0530

    add header ui

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
+
+const Header = () => {
+  const navigate = useNavigate(); // provided by react-router-dom to navigate between routes and return a funtion
+  const user = true;
+
+  return (
+    <nav className="py-4 flex items-center justify-between">
+      <Link to="/">
+        <img src="/logo.png" className="h-16" alt="Logo" />
+      </Link>
+
+      <div>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar className="w-10 rounded-full outline-none" >
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>AP</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Amit Pal</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem> <LinkIcon className="mr-2 h-4 w-4" /> My Links</DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400" > <LogOut className="mr-2 h-4 w-4" /> Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
